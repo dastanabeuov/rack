3a5376d13b182042119d9bb88f53ddf7cec7113b
Dastan Abeuov <d.abeuov9@gmail.com>
2019-02-25 03:32:42 +0600

    2019.02.25

--- application/time_calculator.rb
+++ application/time_calculator.rb
@@ -1,6 +1,5 @@
 class TimeCalculator
-  
-  #мне более понятнее покамись так. стану опытным учту обязательно!
+
   FORMATTED_VERSION = {
     'year' => '%Y',
     'month' => '%m',
@@ -12,25 +11,19 @@ class TimeCalculator
 
   def initialize(formats)
     @formats = formats.split(',')
-    #@format_time = ''
   end
 
   def valid? #все ли параметры верны
-    return true if @formats.select { |i| i == FORMATTED_VERSION.keys.all? }
+    return true if @formats.select { |format| FORMATTED_VERSION.keys.include?(format) }
   end
 
   def unknown_formats #список неверных форматов
-    @unknown_formats = []
-    @formats.each do |f| 
-      @unknown_formats.push(f) unless FORMATTED_VERSION.keys.include?(t)
-    end
+    unknown_formats = @formats.select { |format| format != FORMATTED_VERSION.keys }
   end
 
   def formatted_time #отформатированное время
-    #@formats.each { |t| @format_time += "#{FORMATTED_VERSION[t]}-" }
-    #Time.now.strftime(@format_time).chop! + "\n"
-    @formats.map { |format| FORMATTED_VERSION[format] }.join('-')
-    Time.now.strftime(@formats)
+    format_time = @formats.map { |format| FORMATTED_VERSION[format] }.join('-')
+    Time.now.strftime(format_time)
   end
 
 end
